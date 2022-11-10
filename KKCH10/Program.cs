1d7886368b63023fdc9bfa4d5b1862d91233c09a
Нурлан <@->
2022-11-10 21:13:08 +0300

    Добавьте файлы проекта.

---
+++ KKCH10/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace KKCH10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        rec:
+            Console.Clear();
+            try
+            {
+
+                Console.WriteLine("Напишите число");
+                Double A = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine($"{Math.Pow(A, 2)} " + $"{Math.Pow(A, 3)} " + $"{Math.Pow(A, 4)}");
+                Console.ReadKey();
+
+            }
+            catch
+            {
+                Console.WriteLine("Введите целое число");
+                Console.ReadKey();
+            }
+            goto rec;
+        }
+    }
+}
